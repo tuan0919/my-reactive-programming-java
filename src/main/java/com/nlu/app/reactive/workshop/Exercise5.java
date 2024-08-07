2e2cd2f927b2821f312da8816287d2daa7fa462b
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-08 04:20:08 +0700

    update: done exercise 5

--- src/main/java/com/nlu/app/reactive/workshop/Exercise5.java
+++ src/main/java/com/nlu/app/reactive/workshop/Exercise5.java
@@ -1,5 +1,8 @@
 package com.nlu.app.reactive.workshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
@@ -9,10 +12,24 @@ public class Exercise5 {
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        ReactiveSources.userFlux()
+                .subscribe(System.out::println,
+                        System.err::println, () -> System.out.println("Done"));
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        ReactiveSources.userFlux()
+                .subscribe(new BaseSubscriber<>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        request(1);
+                    }
+
+                    @Override
+                    protected void hookOnNext(User value) {
+                        System.out.println(value.toString() + " received");
+                        request(1);
+                    }
+                });
 
         System.out.println("Press a key to end");
         System.in.read();
