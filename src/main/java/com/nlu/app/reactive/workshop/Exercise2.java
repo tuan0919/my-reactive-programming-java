97197d0bd1cd0b85a091a041476e367955fc1cd9
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-07 23:58:41 +0700

    update: done exercise 2

--- src/main/java/com/nlu/app/reactive/workshop/Exercise2.java
+++ src/main/java/com/nlu/app/reactive/workshop/Exercise2.java
@@ -9,10 +9,13 @@ public class Exercise2 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        ReactiveSources
+                .intNumbersFlux()
+                .subscribe(System.out::println);
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        ReactiveSources.userFlux()
+                .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
