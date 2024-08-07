9285e0ba9934ad378adb59253699aa41d86e3aa8
Nguyễn Quốc Anh Tuấn <147523268+tuan0919@users.noreply.github.com>
2024-08-08 01:57:27 +0700

    update: done exercise 3

--- src/main/java/com/nlu/app/reactive/workshop/Exercise3.java
+++ src/main/java/com/nlu/app/reactive/workshop/Exercise3.java
@@ -10,11 +10,10 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
-
-
-        System.out.println("Press a key to end");
-        System.in.read();
+        var list = ReactiveSources.intNumbersFlux()
+                .toStream().toList();
+        System.out.println(list);
+        System.out.println(list.size());
     }
 
 }
